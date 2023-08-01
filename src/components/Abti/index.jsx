26dfa60a98b7a496ee80871146596ab58be68c43
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-01 15:43:40 +0900

    FEAT : queryList 추가

--- src/components/Abti/index.jsx
+++ src/components/Abti/index.jsx
@@ -1,8 +1,20 @@
 import icon from '../../assets/armor_icon.png';
 import questionData from '../../data/QuestionList.json';
 
-export default function Abti({type = "pc"}) {
-    const testarr = questionData.Computer.query;
+export default function Abti({type = "pc", page, handleNext}) {
+    const queryList = [];
+
+    const ExerciseQ = questionData.Exercise.query;
+    const ReadingQ = questionData.Reading.query;
+    const LinguisticsQ = questionData.Linguistics.query;
+    const FinanceQ = questionData.Finance.query;
+    const ComputerQ = questionData.Computer.query;
+    const MusicQ = questionData.Music.query;
+
+    queryList.push(ExerciseQ, ReadingQ, LinguisticsQ, FinanceQ,
+        ComputerQ, MusicQ);
+
+
     return (
         <div className={type + '-abti'}>
             <div className={type + '-abti-title-box'}>
@@ -12,7 +24,7 @@ export default function Abti({type = "pc"}) {
                 </div>
                 <p>군비티아이를 통해 적절한 자기계발을 추천 해드립니다.</p>
             </div>
-            {testarr.map((query, number) => {
+            {queryList[page].map((query, number) => {
                 return (
                     <div className={type + '-abti-question-box'} key={number}>
                         <div className={type + '-abti-question'}>Q{number + 1}. {query}</div>
@@ -33,7 +45,7 @@ export default function Abti({type = "pc"}) {
             })}
             
             
-            <button className={type + '-abti-button'}>다음&#62;</button>
+            <button className={type + '-abti-button'} onClick={handleNext}>다음&#62;</button>
         </div>
     )
 }

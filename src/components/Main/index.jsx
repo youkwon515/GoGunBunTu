94356d094b81748f8c97952f5ae7215dd65fa6ed
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-01 15:53:04 +0900

    FIX : Main 수정

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,14 +2,16 @@ import Abti from "../Abti";
 import { useState } from "react";
 export default function Main({type = "pc"}) {
     const [next, setNext] = useState(0);
+    let number = -1;
     const handleNext = () => {
         let nextPage = next + 1;
         setNext(nextPage);
     }
 
+
     return (
         <main>
-            <Abti type={type} page={next} handleNext={handleNext}/>
+            <Abti type={type} number={number} page={next} handleNext={handleNext}/>
         </main>
     );
 }

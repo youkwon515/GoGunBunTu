26cb2d364031bc16471c7576bc0c1df4b9dce96c
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-01 15:43:59 +0900

    FEAT : handleNext 기능 추가

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,15 @@
 import Abti from "../Abti";
-
+import { useState } from "react";
 export default function Main({type = "pc"}) {
+    const [next, setNext] = useState(0);
+    const handleNext = () => {
+        let nextPage = next + 1;
+        setNext(nextPage);
+    }
+
     return (
         <main>
-            <Abti type={type}/>
+            <Abti type={type} page={next} handleNext={handleNext}/>
         </main>
     );
 }

d6bb7287e99aba080c876606041c18f85d20e1c4
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-01 22:24:52 +0900

    FIX : Abti 수정

--- src/components/Abti/index.jsx
+++ src/components/Abti/index.jsx
@@ -1,51 +1,87 @@
-import icon from '../../assets/armor_icon.png';
-import questionData from '../../data/QuestionList.json';
+import Title from "../Title";
 
-export default function Abti({type = "pc", number, page, handleNext}) {
-    const queryList = [];
-    const ExerciseQ = questionData.Exercise.query;
-    const ReadingQ = questionData.Reading.query;
-    const LinguisticsQ = questionData.Linguistics.query;
-    const FinanceQ = questionData.Finance.query;
-    const ComputerQ = questionData.Computer.query;
-    const MusicQ = questionData.Music.query;
+export default function Abti({type = "pc", queryList, num, page, handleSubmit, handlePoint, handleNext}) {
+    let result = {
+        "yes1" : null,
+        "yes2" : null,
+        "yes3" : null
+    };
+    let point = 0;
 
-    queryList.push(ExerciseQ, ReadingQ, LinguisticsQ, FinanceQ,
-        ComputerQ, MusicQ);
+    const handleSelect = (e) => {
+        let htmlFor = e.target.htmlFor;
+        if (htmlFor.includes('yes')) {
+            let yesSelete = htmlFor.split('abti-')[1];
+            result[yesSelete] = true;
+        } else {
+            let noSelete = htmlFor.split('abti-no')[1];
+            result["yes" + noSelete] = false;
+        }
+    };
+
+    const pointChecker = () => {
+        if (result["yes1"] === true) {
+            point += 1;
+        }
+
+        if(result["yes2"] === true) {
+            point += 1;
+        }
+
+        if(result["yes3"] === true) {
+            point += 1;
+        }
+
+        handlePoint(point);
+    }
+
+    const handleChecker1 = () => {
+        if (result["yes1"] !== null && result["yes2"] !== null && result["yes3"] !== null) {
+            pointChecker();
+            handleNext();
+        } else {
+            alert('항목을 체크해주세요');
+        }
+    }
+
+    const handleChecker2 = () => {
+        if (result["yes1"] !== null && result["yes2"] !== null && result["yes3"] !== null) {
+            pointChecker();
+            handleSubmit(true);
+        } else {
+            alert('항목을 체크해주세요');
+        }
+    }
+
+    let number = num;
 
 
     return (
         <div className={type + '-abti'}>
-            <div className={type + '-abti-title-box'}>
-                <div className='flex'>
-                    <h2 className={type + '-abti-title'}>군비티아이</h2>
-                    <img src={icon} alt="icon" />
-                </div>
-                <p>군비티아이를 통해 적절한 자기계발을 추천 해드립니다.</p>
-            </div>
-            {queryList[page].map((query) => {
+            <Title type={type} />
+            {queryList[page].map((query, idx) => {
                 number += 1;
+                idx += 1;
                 return (
                     <div className={type + '-abti-question-box'} key={number}>
-                        <div className={type + '-abti-question'}>Q{number + 1}. {query}</div>
+                        <div className={type + '-abti-question'}>Q{number}. {query}</div>
                         <div className={type + '-abti-checkbox'}>
                             <div>
-                                <input type='radio' name={'abti' + number} id={'abti-yes' + number}/>
-                                <label className='abti-yes-label' htmlFor={'abti-yes' + number}></label>
-                                <label className='abti-yes-label' htmlFor={'abti-yes' + number}>동의</label>
+                                <input type='radio' name={'abti' + idx} id={'abti-yes' + idx}/>
+                                <label className='abti-yes-label' onClick={handleSelect} htmlFor={'abti-yes' + idx}></label>
+                                <label className='abti-yes-label' htmlFor={'abti-yes' + idx}>동의</label>
                             </div>
                             <div>
-                                <input type='radio' name={'abti' + number} id={'abti-no' + number}/>
-                                <label className='abti-no-label' htmlFor={'abti-no' + number}></label>
-                                <label className='abti-no-label' htmlFor={'abti-no' + number}>비동의</label>
+                                <input type='radio' name={'abti' + idx} id={'abti-no' + idx}/>
+                                <label className='abti-no-label' onClick={handleSelect} htmlFor={'abti-no' + idx}></label>
+                                <label className='abti-no-label' htmlFor={'abti-no' + idx}>비동의</label>
                             </div>
                         </div>
                     </div>
                 );
             })}
-            
-            
-            <button className={type + '-abti-button'} onClick={handleNext}>다음&#62;</button>
+
+            {page === 6 ? <button className={type + '-abti-button'} onClick={handleChecker2}>완료&#62;</button> : <button className={type + '-abti-button'} onClick={handleChecker1}>다음&#62;</button>}
         </div>
     )
 }

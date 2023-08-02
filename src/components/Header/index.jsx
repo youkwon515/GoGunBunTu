98a8b5be2abe732264e0b9b1737b336fa8a9d7ad
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-02 13:25:45 +0900

    FEAT : border-width 생성

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,16 @@
 import logo from '../../assets/logo.png';
 
-export default function Header({type = "pc"}) {
+export default function Header({type = "pc", borderWidth}) {
+    document.documentElement.style.setProperty(
+        '--borderWidth', 
+        borderWidth + "%"
+    );
     return (
         <header className={type + "-header"}>
             <img src={logo} alt='logo'/>
+            <div className='borderWidth-box'>
+                <div className='borderWidth'></div>
+            </div>
         </header>
 
     )

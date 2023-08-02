163cd23b52fc394ea409834b98fd84131acdc087
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-02 13:24:15 +0900

    FEAT : handleNext, handlePoint, handleResultSet, handleSetBorderWidth

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,43 @@ import React from "react"
 import { useMediaQuery } from "react-responsive"
 import Desktop from "./components/Desktop";
 import Mobile from "./components/Mobile";
+import { useState } from "react";
 function App() {
+  const [submit, setSubmit] = useState(false);
+  const [resultCategory, setResultCategory] = useState('');
+  const [categoryNumber, setCategoryNumber] = useState(0);
+  const [result, setResult] = useState([]);
+  const [next, setNext] = useState(0);
+  const [number, setNumber] = useState(0);
+  const [borderWidth, setBorderWidth] = useState(0);
+
+  const handleNext = () => {
+    let nextPage = next + 1;
+    let numSet = number + 3;
+    handleSetBorderWidth();
+    setNumber(numSet);
+    setNext(nextPage);
+  }
+
+  const handlePoint = (point) => {
+    setResult([...result, point]);
+    console.log(result)
+  }
+
+  const handleResultSet = (result, resultNum) => {
+    
+    setResultCategory(result);
+    setCategoryNumber(resultNum)
+  }
+
+  const handleSetBorderWidth = () => {
+    let width = borderWidth;
+    width += 15;
+    if (width > 100) {
+      width -= (width - 100);
+    }
+    setBorderWidth(width);
+  }
 
   const isPc = useMediaQuery({
     query : "(min-width:767px)"
@@ -16,8 +52,8 @@ function App() {
   return (
   <>
     
-    {isPc && <Desktop />}
-    {isMobile && <Mobile />}
+    {isPc && <Desktop setBorderWidth={handleSetBorderWidth} borderWidth={borderWidth} categoryNumber={categoryNumber} handlePoint={handlePoint} handleNext={handleNext} number={number} next={next} submit={submit} setSubmit={setSubmit} resultCategory={resultCategory} handleResultSet={handleResultSet} result={result} setResult={setResult}/>}
+    {isMobile && <Mobile setBorderWidth={handleSetBorderWidth} borderWidth={borderWidth} categoryNumber={categoryNumber} handlePoint={handlePoint} handleNext={handleNext} number={number} next={next} submit={submit} setSubmit={setSubmit} resultCategory={resultCategory} handleResultSet={handleResultSet}  result={result} setResult={setResult}/>}
   </>
   );
 }

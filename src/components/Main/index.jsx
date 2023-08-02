e7501bdd20810bb8c530a0e648b58b234a290294
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-02 13:26:34 +0900

    FIX : Main 수정

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,31 +1,17 @@
 import Abti from "../Abti";
 import Result from "../Result";
-import { useState } from "react";
 import data from '../../data/QuestionList.json';
 
-export default function Main({type = "pc"}) {
-    const [next, setNext] = useState(0);
-    const [number, setNumber] = useState(0);
-    const [result, setResult] = useState([]);
-    const [submit, setSubmit] = useState(false);
+export default function Main({type, setBorderWidth, categoryNumber, handlePoint, handleNext, number, next, submit, setSubmit, resultCategory, handleResultSet, result}) {
+    
     const queryList = [];
     const certificateList = [];
     const jobList = [];
 
     const maxResult = [];
+
     let category = '';
     let categoryNum = 0;
-
-    const handlePoint = (point) => {
-        setResult([...result, point]);
-    }
-
-    const handleNext = () => {
-        let nextPage = next + 1;
-        let numSet = number + 3;
-        setNumber(numSet);
-        setNext(nextPage);
-    }
 
     const ExerciseQ = data.Exercise.query;
     const ReadingQ = data.Reading.query;
@@ -33,7 +19,7 @@ export default function Main({type = "pc"}) {
     const FinanceQ = data.Finance.query;
     const ComputerQ = data.Computer.query;
     const MusicQ = data.Music.query;
-    const BonusQ = data.Bonus.query;
+    const TeachQ = data.Teach.query;
 
     const Ecertificate = data.Exercise.certificate;
     const Rcertificate = data.Reading.certificate;
@@ -41,6 +27,7 @@ export default function Main({type = "pc"}) {
     const Fcertificate = data.Finance.certificate;
     const Ccertificate = data.Computer.certificate;
     const Mcertificate = data.Music.certificate;
+    const Tcertificate = data.Teach.certificate;
 
     const Ejob = data.Exercise.job;
     const Rjob = data.Reading.job;
@@ -48,52 +35,57 @@ export default function Main({type = "pc"}) {
     const Fjob = data.Finance.job;
     const Cjob = data.Computer.job;
     const Mjob = data.Music.job;
+    const Tjob = data.Teach.job;
 
-    queryList.push(ExerciseQ, ReadingQ, LinguisticsQ, FinanceQ, ComputerQ, MusicQ, BonusQ);
-    certificateList.push(Ecertificate, Rcertificate, Lcertificate, Fcertificate, Ccertificate, Mcertificate);
-    jobList.push(Ejob, Rjob, Ljob, Fjob, Cjob, Mjob);
+
+    queryList.push(ExerciseQ, ReadingQ, LinguisticsQ, FinanceQ, ComputerQ, MusicQ, TeachQ);
+    certificateList.push(Ecertificate, Rcertificate, Lcertificate, Fcertificate, Ccertificate, Mcertificate, Tcertificate);
+    jobList.push(Ejob, Rjob, Ljob, Fjob, Cjob, Mjob, Tjob);
 
     if (submit) {
+        setBorderWidth();
         const max = Math.max(...result);
-
+        console.log(result)
         for (let i = 0; i < result.length; i++) {
-            if (max === result[i]) maxResult.push(i);
+            if (max === result[i]) {maxResult.push(i);}
         }
 
-        maxResult.forEach(e => {
-            if (e === 0) {
-                category = '운동';
-                categoryNum = e;
-            } else if (e === 1) {
-                category = '독서';
-                categoryNum = e;
-            } else if (e === 2) {
-                category = '어학';
-                categoryNum = e;
-            } else if (e === 3) {
-                category = '금융';
-                categoryNum = e;
-            } else if (e === 4) {
-                category = '컴퓨터';
-                categoryNum = e;
-            } else if (e === 5) {
-                category = '음악';
-                categoryNum = e;
-            }
-        });                    
+        const random_recommend = Math.floor(Math.random() * maxResult.length);
+        let recommend = maxResult[random_recommend];
 
-        if (maxResult.length === 7) {
-            category = '만능'; 
+        let count1 = result.filter(element => 3 === element).length;
+        let count2 = result.filter(element => 0 === element).length;
+        
+        if (recommend === 0) {
+            category = '운동';
+        } else if (recommend === 1) {
+            category = '독서';
+        } else if (recommend === 2) {
+            category = '어학';
+        } else if (recommend === 3) {
+            category = '금융';
+        } else if (recommend === 4) {
+            category = '컴퓨터';
+        } else if (recommend === 5) {
+            category = '음악';
+        }
+        
+        categoryNum = recommend;
+        
+
+        if (count1 === 7) {
+            category = '만능';
+        } else if (count2 === 7) {
+            category = '재능이 없다..';
         }
 
-       
-
+        handleResultSet(category, categoryNum);
     }
 
     return (
         <main>
             {submit || <Abti type={type} queryList={queryList} num={number} page={next} handleSubmit={setSubmit} handlePoint={handlePoint} handleNext={handleNext}/>}
-            {submit && <Result type={type} result={category} certificateList={certificateList[categoryNum]} jobList={jobList[categoryNum]}/>}
+            {submit && <Result type={type} Rresult={result} result={resultCategory} certificateList={certificateList[categoryNumber]} jobList={jobList[categoryNumber]}/>}
         </main>
     );
 }

667f8a92c47e0acd83137d8fee111eb692e2d7ab
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-02 13:25:21 +0900

    FIX : Desktop 수정

--- src/components/Desktop/index.jsx
+++ src/components/Desktop/index.jsx
@@ -1,12 +1,12 @@
 import Header from "../Header";
 import Main from "../Main";
 
-export default function Desktop() {
+export default function Desktop({setBorderWidth, borderWidth, categoryNumber, handlePoint, handleNext, number, next, submit, setSubmit, resultCategory, handleResultSet, result, setResult}) {
     let type = 'pc';
     return (
         <>
-            <Header type={type}/>
-            <Main type={type}/>
+            <Header type={type} borderWidth={borderWidth}/>
+            <Main type={type} setBorderWidth={setBorderWidth} categoryNumber={categoryNumber} handlePoint={handlePoint} handleNext={handleNext} number={number} next={next} submit={submit} setSubmit={setSubmit} resultCategory={resultCategory} handleResultSet={handleResultSet} result={result} setResult={setResult}/>
         </>
     )
 }

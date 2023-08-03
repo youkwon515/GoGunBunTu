747f6ce5b2bfa1a0621b22b0af4026f6b29c757e
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-04 02:00:17 +0900

    FIX : main 수정

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -16,20 +16,19 @@ function MainPage() {
                     <Header type={is} borderWidth={0}/>
                     <S.Background src={bg} alt="background" />
                     <S.Text>
-                        <h1>군인들의 자기개발을 위한<br/>TODO LIST</h1>
+                        <h1>군인들의 자기계발을 위한<br/>TODO LIST</h1>
                         <p>For more effective self-development!</p>
                     </S.Text>
+                    <div className={is + "-router-box main-btn-box"}>
+                        <Link to="/abti"><button className={is + "-router-button"}>군비티아이 &#62;</button></Link>
+                        <Link to="/todoList"><button className={is + "-router-button"}>투두리스트 &#62;</button></Link>
+                        <Link to="/testSite"><button className={is + "-router-button"}>시험장 검색 &#62;</button></Link>
+                    </div>
                 </>
             ) : (
                 <h2>모바일</h2>
             )
         }
-        
-        <div className={is + "-router-box main-btn-box"}>
-            <Link to="/abti"><button className={is + "-router-button"}>군비티아이 &#62;</button></Link>
-            <Link to="/"><button className={is + "-router-button"}>투두리스트 &#62;</button></Link>
-            <Link to="/testSite"><button className={is + "-router-button"}>시험장 찾기 &#62;</button></Link>
-        </div>
         </Main>
     )
 }

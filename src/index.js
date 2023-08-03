aaf8e64020cc7d672a98be01c07718a2869759df
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-08-03 15:40:06 +0900

    FIX : 경로 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Router from './pages/router';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Router />);
